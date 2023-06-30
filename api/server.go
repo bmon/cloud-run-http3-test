fb7608d9df4d869a8e5d4254e8ad56ac21a69fc6
Brendan Roy <br3ndanr@gmail.com>
2023-06-30 15:38:00 +1000

    implement h3 test app

--- api/server.go
+++ api/server.go
@@ -1,7 +1,9 @@
 package api
 
 import (
-	"fmt"
+	_ "embed"
+	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 
@@ -14,22 +16,54 @@ func NewServer() *API {
 	return &API{}
 }
 
+func logAccess(h http.HandlerFunc) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		h(rw, r)
+		log.Printf("served %s", r.URL)
+	}
+}
+
 func (a *API) Handler() http.Handler {
 	// Setup request router.
 	r := mux.NewRouter()
-	r.HandleFunc("/", a.HandleIndex).Methods("GET")
-	r.HandleFunc("/client-ping", a.HandleClientPing).Methods("POST")
+	r.HandleFunc("/", logAccess(a.HandleIndex)).Methods("GET")
+	r.HandleFunc("/client-ping", logAccess(a.HandleClientPing)).Methods("POST")
 
 	return r
 }
 
+//go:embed index.html
+var indexHTML []byte
+
 func (a *API) HandleIndex(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusOK)
-	rw.Write([]byte("<h1>Hello world!</h1>"))
+	rw.Write(indexHTML)
 }
 
 func (a *API) HandleClientPing(rw http.ResponseWriter, r *http.Request) {
 	serverTime := time.Now().UnixMilli()
+	res := struct {
+		ServerTime int64  `json:"server_time"`
+		ClientIP   string `json:"client_ip"`
+	}{serverTime, clientIP(r)}
+
+	data, err := json.Marshal(res)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write([]byte(err.Error()))
+	}
+
 	rw.WriteHeader(http.StatusOK)
-	rw.Write([]byte(fmt.Sprintf(`{"server_time": %d}`, serverTime)))
+	rw.Write(data)
+}
+
+func clientIP(req *http.Request) string {
+	addr := req.Header.Get("X-Real-Ip")
+	if addr == "" {
+		addr = req.Header.Get("X-Forwarded-For")
+	}
+	if addr == "" {
+		addr = req.RemoteAddr
+	}
+	return addr
 }
